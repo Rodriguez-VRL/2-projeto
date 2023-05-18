ab25076e0e7af378e2482366be1fc29a22d2ae24
Rodriguez-VRL <vinicius.r.luiz@gmail.com>
2023-05-18 20:53:28 -0300

    filter

--- assets/crud.js
+++ assets/crud.js
@@ -1,47 +1,97 @@
 document.querySelector("#salvar").addEventListener("click",cadastrar)
 
+
+let tarefas = []
+
+window.addEventListener("load", () => {
+    tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+    atualizar()
+})
+
+document.querySelector("#busca").addEventListener("keyup", ()=> {
+    let busca = document.querySelector("#busca").value
+    let tarefasFiltradas = tarefas.filter((tarefa) =>{
+        return tarefa.titulo.toLowerCase().includes(busca.toLowerCase())
+    })
+    filtrar(tarefasFiltradas)
+})
+
+function filtrar(tarefas){
+    document.querySelector("#tarefas").innerHTML = ""
+    tarefas.forEach((tarefa) =>{
+        document.querySelector("#tarefas").innerHTML 
+                    += createCard(tarefa)
+    })
+}
+
+function atualizar(){
+    document.querySelector("#tarefas").innerHTML = ""
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    tarefas.forEach((tarefa) =>{
+        document.querySelector("#tarefas").innerHTML 
+                    += createCard(tarefa)
+    })
+}
+
+
+
 function cadastrar(){
     const titulo = document.querySelector("#titulo").value
     const descricao = document.querySelector("#descricao").value
     const categoria = document.querySelector("#categoria").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-
-
-    const tarefa = { /* Quando o titulo for igual a variavel, não precisa por (titulo:titulo) pode ser direto */
+    const tarefa = {
+        id: Date.now(),
         titulo,
         descricao,
         categoria,
+        concluida: false
     }
-
-    if(!validar(tarefa.titulo, document.querySelector("#titulo"))) return
-    if(!validar(tarefa.descricao, document.querySelector("#descricao"))) return
-
-    document.querySelector("#tarefas").innerHTML += createCard (tarefa) /*O card será colocado dentro do HTML*/ 
     
-    modal.hide()
+    if (!validar(tarefa.titulo, document.querySelector("#titulo"))) return
+    if (!validar(tarefa.descricao, document.querySelector("#descricao"))) return
+    
+    tarefas.push(tarefa)    
+    
+    atualizar()
 
+    modal.hide()
 
 }
 
 function validar(valor, campo){
     if(valor == ""){
-        campo.classList.add ("is-invalid") /*Mostrando que está invalido */
-        campo.classList.remove ("is-valid") 
+        campo.classList.add("is-invalid")
+        campo.classList.remove("is-valid")
         return false
     }
 
-    campo.classList.remove ("is-invalid") /* Removendo o invalido*/
-    campo.classList.add ("is-valid")/* Colocando que está valido*/
+    campo.classList.remove("is-invalid")
+    campo.classList.add("is-valid")
     return true
     
 }
 
-function apagar(botao){
-    botao.parentNode.parentNode.parentNode. remove()
+function apagar(id){
+
+    tarefas = tarefas.filter((tarefa) => {
+        return tarefa.id != id
+    })
+    atualizar()
+ 
+}
+
+function concluir(id){
+    let tarefaEncontrada = tarefas.find((tarefa) => {
+        return tarefa.id == id
+    })
+    tarefaEncontrada.concluida = true
+    atualizar()
 }
 
 function createCard(tarefa){
+    let disabled = tarefa.concluida ? "disabled" : ""
     return `
             <div class="col-lg-3 col-md-6 col-12">
                 <div class="card">
@@ -53,10 +103,10 @@ function createCard(tarefa){
                         <p>
                             <span class="badge text-bg-warning">${tarefa.categoria}</span>
                         </p>
-                        <a href="#" class="btn btn-success">
+                        <a onClick="concluir(${tarefa.id})" href="#" class="btn btn-success ${disabled}">
                             <i class="bi bi-check-lg"></i>
                         </a>
-                        <a onClick = "apagar(this)" href="#" class="btn btn-danger">
+                        <a onClick="apagar(${tarefa.id})" href="#" class="btn btn-danger">
                             <i class="bi bi-trash"></i>
                         </a>
                     </div>
